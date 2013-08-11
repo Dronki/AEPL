0eb716762e8ee515d7de388e32e165b316272005
Rickard Ahlstedt <rickard.re.ahlstedt@gmail.com>
2013-08-11 13:28:23 +0200

    Renamed File

---
+++ src/com/rickythemaniac/AEPL/AEPL.java
@@ -0,0 +1,21 @@
+package com.rickythemaniac.AEPL;
+
+import java.io.File;
+
+/**
+ * @author Rickard Ahlstedt
+ * @version 0.01
+ * @since 2013-08-06
+ */
+
+public class AEPL {
+
+	public static void main(String[] args) {
+		if(args.length >= 0) {
+			System.err.println("Please specify a file to convert!");
+			System.exit(1);
+		}
+		
+	}
+	
+}
