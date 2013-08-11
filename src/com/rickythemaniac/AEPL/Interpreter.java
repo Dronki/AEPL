427f59a0385d85e7b2d2b22a471f4f03baf5625e
Rickard Ahlstedt <rickard.re.ahlstedt@gmail.com>
2013-08-11 13:25:24 +0200

    Added a sample file

--- src/com/rickythemaniac/AEPL/Interpreter.java
+++ src/com/rickythemaniac/AEPL/Interpreter.java
@@ -1,5 +1,18 @@
 package com.rickythemaniac.AEPL;
 
+/**
+ * @author Rickard Ahlstedt
+ * @version 0.01
+ * @since 2013-08-06
+ */
+
 public class Interpreter {
 
+	public static void main(String[] args) {
+		if(args.length >= 0) {
+			System.err.println("Please specify a file to convert!");
+			System.exit(1);
+		}
+	}
+	
 }
---
+++ test.eng
@@ -0,0 +1,8 @@
+//Test-file
+
+There is a integer called a, with a value of 5.
+There is a integer called b, with a value of 5.
+
+Function named main, using String[] with name args as parameters.
+	print a plus b.
+End of function main.
